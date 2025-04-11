80b2d361a0e7dc1e5a1246eaa230a3de5bd1f0ec
Vyas <jv624@ic.ac.uk>
2025-04-12 00:54:17 +0100

    this works till transferring liquid from B201 to B204 but not any further

--- code/trial.py
+++ code/trial.py
@@ -100,7 +100,8 @@ if __name__ == "__main__":
     df_history = pd.DataFrame()
 
     
-    for sim_time in range(10):
+    for sim_time in range(15):
+        print(f"Simulation time: {sim_time}")
         with open('mixer_sim.json') as f:
             setup = json.load(f)
         
@@ -116,9 +117,20 @@ if __name__ == "__main__":
             setup['ds1']['model']['modules']['mixer0']['init_states']['valve_pump_tank_B201_input']=valve_pump_tank_B201
             setup['ds1']['model']['modules']['mixer0']['init_states']['valve_pump_tank_B202_input']=valve_pump_tank_B202
             setup['ds1']['model']['modules']['mixer0']['init_states']['valve_pump_tank_B203_input']=valve_pump_tank_B203
+            setup['ds1']['model']['modules']['mixer0']['init_states']['valve_pump_tank_B204_input']=valve_pump_tank_B204
         for i in setup:
             run_sim(sim_setup=setup[i], modus='hybrid', states=True)
         
+        df = pd.read_csv('../data/ds1/ds1_hybrid_s.csv')
+        B201_level = df['mixer0.tank_B201.level'].iloc[-1]
+        B202_level = df['mixer0.tank_B202.level'].iloc[-1]
+        B203_level = df['mixer0.tank_B203.level'].iloc[-1]
+        B204_level = df['mixer0.tank_B204.level'].iloc[-1]
+        B201_level_list.append(df['mixer0.tank_B201.level'].iloc[-1])
+        B202_level_list.append(df['mixer0.tank_B202.level'].iloc[-1])
+        B203_level_list.append(df['mixer0.tank_B203.level'].iloc[-1])
+        B204_level_list.append(df['mixer0.tank_B204.level'].iloc[-1])
+
         res = PlantOperatorCrew().crew().kickoff(inputs={"B201_level": B201_level, 
                                                     "B202_level": B202_level, 
                                                     "B203_level": B203_level, 
@@ -171,18 +183,18 @@ if __name__ == "__main__":
         print(B201_level, B202_level, B203_level, B204_level)
         df_history = pd.concat([df_history,df])
 
-        B201_level = df_history['mixer0.tank_B201.level'].iloc[-1]
-        B202_level = df_history['mixer0.tank_B202.level'].iloc[-1]
-        B203_level = df_history['mixer0.tank_B203.level'].iloc[-1]
-        B204_level = df_history['mixer0.tank_B204.level'].iloc[-1]
-        B201_level_list.append(df_history['mixer0.tank_B201.level'].iloc[-1])
-        B202_level_list.append(df_history['mixer0.tank_B202.level'].iloc[-1])
-        B203_level_list.append(df_history['mixer0.tank_B203.level'].iloc[-1])
-        B204_level_list.append(df_history['mixer0.tank_B204.level'].iloc[-1])
-        setup['ds1']['model']['modules']['mixer0']['init_states']['B201_level']=df_history['mixer0.tank_B201.level'].iloc[-1]
-        setup['ds1']['model']['modules']['mixer0']['init_states']['B202_level']=df_history['mixer0.tank_B202.level'].iloc[-1]
-        setup['ds1']['model']['modules']['mixer0']['init_states']['B203_level']=df_history['mixer0.tank_B203.level'].iloc[-1]
-        setup['ds1']['model']['modules']['mixer0']['init_states']['B204_level']=df_history['mixer0.tank_B204.level'].iloc[-1]
+        # B201_level = df_history['mixer0.tank_B201.level'].iloc[-1]
+        # B202_level = df_history['mixer0.tank_B202.level'].iloc[-1]
+        # B203_level = df_history['mixer0.tank_B203.level'].iloc[-1]
+        # B204_level = df_history['mixer0.tank_B204.level'].iloc[-1]
+        # B201_level_list.append(df_history['mixer0.tank_B201.level'].iloc[-1])
+        # B202_level_list.append(df_history['mixer0.tank_B202.level'].iloc[-1])
+        # B203_level_list.append(df_history['mixer0.tank_B203.level'].iloc[-1])
+        # B204_level_list.append(df_history['mixer0.tank_B204.level'].iloc[-1])
+        setup['ds1']['model']['modules']['mixer0']['init_states']['B201_level']=B201_level#df_history['mixer0.tank_B201.level'].iloc[-1]
+        setup['ds1']['model']['modules']['mixer0']['init_states']['B202_level']=B202_level#df_history['mixer0.tank_B202.level'].iloc[-1]
+        setup['ds1']['model']['modules']['mixer0']['init_states']['B203_level']=B203_level#df_history['mixer0.tank_B203.level'].iloc[-1]
+        setup['ds1']['model']['modules']['mixer0']['init_states']['B204_level']=B204_level#df_history['mixer0.tank_B204.level'].iloc[-1]
         setup['ds1']['model']['modules']['mixer0']['init_states']['valve_in0_input']= 1 if res['valve_in0'] else 0
         setup['ds1']['model']['modules']['mixer0']['init_states']['valve_in1_input']= 1 if res['valve_in1'] else 0
         setup['ds1']['model']['modules']['mixer0']['init_states']['valve_in2_input']= 1 if res['valve_in2'] else 0
@@ -190,6 +202,7 @@ if __name__ == "__main__":
         setup['ds1']['model']['modules']['mixer0']['init_states']['valve_pump_tank_B201_input']=1 if res['valve_pump_tank_B201'] else 0
         setup['ds1']['model']['modules']['mixer0']['init_states']['valve_pump_tank_B202_input']=1 if res['valve_pump_tank_B202'] else 0
         setup['ds1']['model']['modules']['mixer0']['init_states']['valve_pump_tank_B203_input']=1 if res['valve_pump_tank_B203'] else 0
+        setup['ds1']['model']['modules']['mixer0']['init_states']['valve_pump_tank_B204_input']=1 if res['valve_pump_tank_B204'] else 0
         with open("mixer_sim.json", "w") as json_file:
             json.dump(setup, json_file, indent=4)
     df = pd.DataFrame({ 'message': message,
